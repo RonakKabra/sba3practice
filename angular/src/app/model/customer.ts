ea401d180c7bedaaf34efc7fac41743fd33c7a5d
RonakKabra <rronakkabra@gmail.com>
2018-10-24 20:09:35 +0530

    Angular files

---
+++ angular/src/app/model/customer.ts
@@ -0,0 +1,13 @@
+import { Time } from "@angular/common";
+
+export class Customer {
+	
+    public contactId: number;
+	public  firstName:string;
+
+	public  timing:Time;
+
+	public  emailId:string;
+	
+	public  mobileNUmber:string;
+}
---
+++ angular/src/app/model/packages.ts
@@ -0,0 +1,8 @@
+export class Packages {
+
+    public title:string;
+    public net_speed:number;
+    public max_usage:number;
+    public cost:number; 
+
+}
